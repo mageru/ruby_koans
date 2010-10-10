cbc80fc807941681adb883bd7fa873e70416a2d6
Justin Miller <jmiller9@gmail.com>
2010-10-10 19:45:42 -0400

    at 169/269

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,26 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError
+  end
+  
+  # The sum of two sides should be less than the other side
+  if((a+b <= c) || (a+c <= b) || (b+c <= a))
+    raise TriangleError
+  end
+  if a==b && b==c 
+    return :equilateral
+  end
+  if (a==b && a!=c) || (a==c && a!=b) || (b==c && b!=a)
+    return :isosceles
+  end
+  if(a!=b && a!=c && b!=c)
+    return :scalene
+  end
+  
+ 
 end
 
 # Error class used in part 2.  No need to change this code.
